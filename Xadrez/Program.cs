ef5d4f5ef4bd6fe0233716baca55fbc4945d3055
EdHall <halluchroger@gmail.com>
2022-12-14 09:36:08 -0300

    Adicinado as classes Tela, e criando as peças do xadrez(Rei,Torre)

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -11,9 +11,14 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro p = new Tabuleiro(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            
+            tab.colocaPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+            tab.colocaPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+            tab.colocaPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
