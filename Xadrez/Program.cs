76d0993567ffbb1cfc375e63a0703b4ea77843a5
EdHall <halluchroger@gmail.com>
2022-12-20 09:30:28 -0300

    Adicionado excecoes personalizadas

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -11,15 +11,22 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocaPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-            tab.colocaPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-            tab.colocaPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+                tab.colocaPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+                tab.colocaPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+                tab.colocaPeca(new Rei(Cor.Preta, tab), new Posicao(1, 1));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
