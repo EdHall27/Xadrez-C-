da7b3cf19c148f52f4113908231029b15d35a792
EdHall <halluchroger@gmail.com>
2023-01-10 09:36:21 -0300

    Correçao: mudando o nome do metodo para movimentoPossivel

--- Xadrez/Xadrez/PartidaDeXadrez.cs
+++ Xadrez/Xadrez/PartidaDeXadrez.cs
@@ -97,7 +97,7 @@ namespace Xadrez
 
         public void validarPosicaDeDestino(Posicao origem, Posicao destino)
         {
-            if (!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida");
             }
